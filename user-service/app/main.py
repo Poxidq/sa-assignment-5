8d23fa29f9f291ee8f53fa9ea62ebe85666211de
Nikita Sannikov <n.sannikov@innopolis.university>
2024-10-12 23:51:25 +0300

    fix user-service router

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from .routes import app as user_routes
+from .routes import router as user_router  # Import the router
 from .database import init_db
 
 def create_app():
     app = FastAPI()
-    app.include_router(user_routes)
+    app.include_router(user_router)  # Include the router
     init_db()  # Initialize the database tables on startup
     return app
 
