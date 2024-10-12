7bea5563ac4f58c4027293f67705fa3cd5f562e6
Nikita Sannikov <n.sannikov@innopolis.university>
2024-10-12 23:34:46 +0300

    add user-service to docker-compose, connect db url

--- user-service/app/database.py
+++ user-service/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-DATABASE_URL = "postgresql://postgres:password@db/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
